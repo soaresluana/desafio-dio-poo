e3705928a1123349335ce8bb7d27e6a5e2ffd8c8
Luana Soares <luana.gabsoares@gmail.com>
2023-02-17 11:27:10 -0300

    abstraindo e encapsulando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso PHP");
+        curso2.setDescricao("Descrição curso PHP");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
